d0a8dabbf75f7c317edbdcbae1bf66d729ce760f
mcataford <c.marcandre@gmail.com>
2017-02-28 23:05:46 -0500

    Added temp. port map + process stub.

--- CPUImplementation/ALU.vhdl
+++ CPUImplementation/ALU.vhdl
@@ -4,10 +4,26 @@ use ieee.std_logic_1164.all;
 
 entity ALU is
 
+--TODO: determine ALU_CONTROL width.
+
+port(
+	CLOCK: in std_logic;
+	A,B: in std_logic_vector(31 downto 0);
+	ALU_CONTROL: in std_logic_vector(31 downto 0);
+	OUTPUT: out std_logic_vector(31 downto 0);
+	ZERO: out std_logic;
+);
+
 end entity;
 
 architecture ALU_impl of ALU is
 
 begin
+
+process(CLOCK)
+
+begin 
+
+end process;
 
 end architecture;
