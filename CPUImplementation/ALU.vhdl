3064dd6280ed860d51f33acc3846baf6f6ae080d
mcataford <c.marcandre@gmail.com>
2017-03-02 21:44:45 -0500

    WFA instantiation + int. signals.

--- CPUImplementation/ALU.vhdl
+++ CPUImplementation/ALU.vhdl
@@ -18,12 +18,30 @@ end entity;
 
 architecture ALU_impl of ALU is
 
+--Intermediate signals, WFA interactions.
+signal WFA_A, WFA_B, WFA_S: std_logic_vector(31 downto 0) := (others => '0');
+signal WFA_Cout: std_logic := '0';
+
+component WORDFULLADDER
+
+port(
+	A,B: in std_logic_vector(31 downto 0);
+	Cout: out std_logic;
+	S: out std_logic_vector(31 downto 0)
+);
+
+end component;
+
 begin
+
+--Word-width full adder component instance.
+WFA: WORDFULLADDER port map(WFA_A,WFA_B,WFA_Cout,WFA_S);
 
 process(CLOCK)
 
 begin 
 
+--Synchronized block
 if rising_edge(CLOCK) then
 
 end if;
