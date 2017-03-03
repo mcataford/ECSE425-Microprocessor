ccdccde64f83083facce56f067ad77596f534822
mcataford <c.marcandre@gmail.com>
2017-03-02 21:38:36 -0500

    Tested with 2's complement on select cases, OK.

--- CPUImplementation/WORDFULLADDER_tb.vhdl
+++ CPUImplementation/WORDFULLADDER_tb.vhdl
@@ -29,25 +29,30 @@ process
 
 begin
 
-A <= std_logic_vector(to_unsigned(50,32));
+A <= std_logic_vector(to_unsigned(52,32));
 
-B <= std_logic_vector(to_unsigned(50,32));
+--B <= not std_logic_vector(to_unsigned(50,32));
+B <= "11111111111111111111111111001110";
+
 
 wait for 1 ns;
 
-for i in 0 to INTEGER'HIGH loop
+--Full range testing, requires extensive resources and time for simulation.
+--for i in 0 to INTEGER'HIGH loop
+--
+--	A <= std_logic_vector(to_unsigned(i,32));
+--
+--	for j in 0 to INTEGER'HIGH loop
+--
+--	B <= std_logic_vector(to_unsigned(j,32));
+--
+--	wait for 1 ns;
+--
+--	end loop;
+--
+--end loop;
 
-	A <= std_logic_vector(to_unsigned(i,32));
-
-	for j in 0 to INTEGER'HIGH loop
-
-	B <= std_logic_vector(to_unsigned(j,32));
-
-	wait for 1 ns;
-
-	end loop;
-
-end loop;
+wait;
 
 end process;
 
