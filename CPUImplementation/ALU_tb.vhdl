7163efb28a7ba7df2120f34bc2dabb9ec326a4b1
mcataford <c.marcandre@gmail.com>
2017-03-05 16:32:09 -0500

    Removed clock, purely combinatorial design.

--- CPUImplementation/ALU_tb.vhdl
+++ CPUImplementation/ALU_tb.vhdl
@@ -9,7 +9,7 @@ end entity;
 architecture ALU_tst of ALU_tb is
 
 signal A,B,OUTPUT: std_logic_vector(31 downto 0);
-signal CLOCK,ZERO,OVERFLOW: std_logic;
+signal ZERO,OVERFLOW: std_logic;
 signal ALU_CONTROL: std_logic_vector(2 downto 0);
 
 constant clock_period: time := 1 ns;
@@ -18,8 +18,6 @@ component ALU
 
 --To be replace with future versions of the port map.
 port(
-	--Clock signal
-	CLOCK: in std_logic;
 	--Inputs
 	A,B: in std_logic_vector(31 downto 0);
 	ALU_CONTROL: in std_logic_vector(2 downto 0);
@@ -31,18 +29,7 @@ end component;
 
 begin
 
-ALU_tst: ALU port map(CLOCK,A,B,ALU_CONTROL,OUTPUT,ZERO,OVERFLOW);
-
-process
-
-begin
-
-CLOCK <= '0';
-wait for 0.5 * clock_period;
-CLOCK <= '1';
-wait for 0.5 * clock_period;
-
-end process;
+ALU_tst : ALU port map(A,B,ALU_CONTROL,OUTPUT,ZERO,OVERFLOW);
 
 process
 
@@ -51,8 +38,6 @@ begin
 A <= std_logic_vector(to_unsigned(50,32));
 B <= std_logic_vector(to_unsigned(60,32));
 ALU_CONTROL <= "000";
-
-wait for 1 * clock_period;
 
 wait;
 
