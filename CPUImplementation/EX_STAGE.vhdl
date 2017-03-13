9e5086b16e197cff2b32a8a92ccdba022583ec9c
mcataford <c.marcandre@gmail.com>
2017-03-13 14:16:59 -0400

    Stub + port map for EX stage.

---
+++ CPUImplementation/EX_STAGE.vhdl
@@ -0,0 +1,23 @@
+library IEEE;
+
+use ieee.std_logic_1164.all;
+
+entity EX_STAGE is
+
+port(
+
+A,B,I,Ins,PC: in std_logic_vector(31 downto 0);
+BRANCH: out std_logic;
+R,B,Ins: out std_logic_vector(31 downto 0)
+
+);
+
+end entity;
+
+architecture EX_STAGE_Impl is EX_STAGE is
+
+begin
+
+
+
+end architecture;
