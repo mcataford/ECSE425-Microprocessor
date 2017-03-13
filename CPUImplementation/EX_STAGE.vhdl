f61dbdd54fe7b166a9bb93b820c5ba3f53f1e178
mcataford <c.marcandre@gmail.com>
2017-03-13 14:58:38 -0400

    Clean up.

--- CPUImplementation/EX_STAGE.vhdl
+++ CPUImplementation/EX_STAGE.vhdl
@@ -7,17 +7,37 @@ entity EX_STAGE is
 port(
 
 A,B,I,Ins,PC: in std_logic_vector(31 downto 0);
+ALU_CONTROL: in std_logic_vector(2 downto 0);
 BRANCH: out std_logic;
-R,B,Ins: out std_logic_vector(31 downto 0)
+R,FB,FIns: out std_logic_vector(31 downto 0)
 
 );
 
 end entity;
 
-architecture EX_STAGE_Impl is EX_STAGE is
+architecture EX_STAGE_Impl of EX_STAGE is
+
+signal STATUS: std_logic_vector(2 downto 0) := "000";
+
+
+component ALU
+
+port(
+	--Inputs
+	A,B: in std_logic_vector(31 downto 0);
+	ALU_CONTROL: in std_logic_vector(2 downto 0);
+	OUTPUT: out std_logic_vector(31 downto 0);
+	ZERO, OVERFLOW: out std_logic
+);
+
+end component;
 
 begin
 
+ALU_unit : ALU port map(A,B,ALU_CONTROL,R,STATUS(0),STATUS(1));
+
+FB <= B;
+FIns <= Ins;
 
 
 end architecture;
