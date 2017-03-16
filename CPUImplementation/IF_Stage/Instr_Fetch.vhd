ea2f115efdc84f611a6a1eec2a7c4d73252f7daa
mcataford <c.marcandre@gmail.com>
2017-03-15 23:58:08 -0400

    Inst. memory, groundwork for loadprocess.

--- CPUImplementation/IF_Stage/Instr_Fetch.vhd
+++ CPUImplementation/IF_Stage/Instr_Fetch.vhd
@@ -8,25 +8,30 @@ use IEEE.numeric_std.all;
 entity IF_STAGE is
 	port(
 		--Inputs--
-		CLK: in std_logic;
+		CLOCK: in std_logic;
 		RESET: in std_logic;
 		---Control Signals---
-		PC_SELECT: in std_logic; --MUX select
-		PC_ADDR_IN: in std_logic_vector(31 downto 0); --One of the MUX inputs
+		PC_SRC: in std_logic; --MUX select
+		ALU_PC: in std_logic_vector(31 downto 0); --One of the MUX inputs
 		--Outputs--
-		PC_ADDR_OUT: out std_logic_vector(31 downto 0);
+		PC_OUT: out std_logic_vector(31 downto 0);
 		INSTR: out std_logic_vector(31 downto 0)
 	);
 end IF_STAGE;
 
 architecture IF_STAGE_Impl of IF_STAGE is	
 
---Signals--
-signal PC_ADDR_AUX1: std_logic_vector(31 downto 0); --Old PC instruction
-signal PC_ADDR_AUX2: std_logic_vector(32 downto 0); --New PC instruction (and carry out)
-signal PC_ADDR_AUX3: std_logic_vector(31 downto 0); --MUX output  
+signal PC_CURRENT: std_logic_vector(31 downto 0) := (others => '0'); --Old PC instruction
+signal PC_INCR: std_logic_vector(31 downto 0); --New PC instruction (and carry out)
+signal PC_FEEDBACK: std_logic_vector(31 downto 0);
+
+signal INSTR_ADDR: integer := 0;
+signal INSTR_BYTE: std_logic_vector(7 downto 0);
+signal WAIT_REQ: std_logic;
+
 signal INSTR_AUX: std_logic_vector(31 downto 0); --Current instruction
-constant PC_COUNT : STD_LOGIC_VECTOR(31 downto 0) := "00000000000000000000000000000100"; --The integer 4 to be added
+
+signal WFA_Cout: std_logic := '0';
 
 --Components--
 component WORDFULLADDER 
@@ -48,53 +53,51 @@ port (
 end component;
 
 component PC_Register is 
-	generic (N:NATURAL := 32);
 	port(
-		CLK: in std_logic;
+		CLOCK: in std_logic;
 		RESET: in std_logic;
-		DATA_IN: in std_logic_vector(N-1 downto 0);
-		DATA_OUT: out std_logic_vector(N-1 downto 0)
+		REG_WRITE: in std_logic;
+		DATA_IN: in std_logic_vector(31 downto 0);
+		DATA_OUT: out std_logic_vector(31 downto 0)
 	);
 end component PC_Register;
 
-component IF_ID_Register is   
-	port(
-		CLK: in std_logic;
-		RESET: in std_logic;
-		PC_ADDR_IN: in std_logic_vector(31 downto 0);
-		INSTR_IN: in std_logic_vector(31 downto 0);
-		PC_ADDR_OUT: out std_logic_vector(31 downto 0);
-		INSTR_OUT: out std_logic_vector(31 downto 0)
-	);
-end component IF_ID_Register;
+component memory is
+	GENERIC(
+	ram_size : INTEGER := 32768;
+	mem_delay : time := 1 ns;
+	clock_period : time := 1 ns
+);
+	PORT (
+	clock: IN STD_LOGIC;
+	writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+	address: IN INTEGER RANGE 0 TO ram_size-1;
+	memwrite: IN STD_LOGIC;
+	memread: IN STD_LOGIC;
+	readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+	waitrequest: OUT STD_LOGIC
+);
+end component;
 
---component memory is
---  	GENERIC(
---	  ram_size : INTEGER := 32768;
---	  mem_delay : time := 10 ns;
---	  clock_period : time := 1 ns
---   );
---	port(
---		RESET: in std_logic;
--- 			READ_ADDR: in std_logic_vector(31 downto 0);
--- 			INSTR: out std_logic_vector(31 downto 0)
--- 			
--- 			--clock: IN STD_LOGIC;
---	  --writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
---	  --address: IN INTEGER RANGE 0 TO ram_size-1;
---	  --memwrite: IN STD_LOGIC;
---	  --memread: IN STD_LOGIC;
---	  --readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
---	  --waitrequest: OUT STD_LOGIC
---	);
---end component memory;
---
 --Port maps--
 begin
 
-WFA : WORDFULLADDER port map(PC_ADDR_AUX1, PC_COUNT, PC_ADDR_AUX2, WFA_COut);
-PC_REG : PC_Register port map(CLK, RESET, PC_ADDR_AUX3, PC_ADDR_AUX1);
---INSTR_MEM : memory port map();
-IF_ID_REG: IF_ID_REGISTER port map(CLK, RESET, PC_ADDR_AUX2, INSTR_AUX, PC_ADDR_OUT, INSTR);	
+WFA : WORDFULLADDER port map(PC_CURRENT, std_logic_vector(to_unsigned(4,32)), PC_INCR, WFA_Cout);
+PC_REG : PC_Register port map(CLOCK, '0', '1', PC_FEEDBACK, PC_CURRENT);
+MX : MUX port map(PC_INCR, ALU_PC, PC_SRC, PC_FEEDBACK);
+INSTR_MEM : memory port map(CLOCK, std_logic_vector(to_unsigned(0,32)), INSTR_ADDR, '0', '1', INSTR, WAIT_REQ);
+
+process(CLOCK)
+
+begin
+
+if rising_edge(CLOCK) then
+
+	PC_OUT <= PC_FEEDBACK;
+	INSTR_ADDR <= to_integer(unsigned(PC_CURRENT));
+
+end if;
+
+end process;
 
 end architecture;
