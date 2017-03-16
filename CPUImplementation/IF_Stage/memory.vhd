2ed08370a63d4eb4dded4eb84dfc913e6f84d6ba
mcataford <c.marcandre@gmail.com>
2017-03-16 00:31:48 -0400

    Adjusted initialization for 32b lines.

--- CPUImplementation/IF_Stage/memory.vhd
+++ CPUImplementation/IF_Stage/memory.vhd
@@ -33,7 +33,7 @@ BEGIN
 	BEGIN
 		--This is a cheap trick to initialize the SRAM in simulation
 		IF(now < 1 ps)THEN
-			For i in 0 to ram_size-1 LOOP
+			For i in 0 to (ram_size/4)-1 LOOP
 				ram_block(i) <= std_logic_vector(to_unsigned(i,32));
 			END LOOP;
 		end if;
