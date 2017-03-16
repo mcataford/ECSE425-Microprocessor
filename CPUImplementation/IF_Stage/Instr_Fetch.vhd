4543791160aa58b94c89b208d9f25db3f24071a6
mcataford <c.marcandre@gmail.com>
2017-03-16 01:42:30 -0400

    Combining signals /w same format for clarity.

--- CPUImplementation/IF_Stage/Instr_Fetch.vhd
+++ CPUImplementation/IF_Stage/Instr_Fetch.vhd
@@ -21,13 +21,10 @@ end IF_STAGE;
 
 architecture IF_STAGE_Impl of IF_STAGE is	
 
-signal PC_CURRENT: std_logic_vector(31 downto 0) := (others => '0'); --Old PC instruction
-signal PC_INCR: std_logic_vector(31 downto 0); --New PC instruction (and carry out)
-signal PC_FEEDBACK: std_logic_vector(31 downto 0) := (others => '0');
-signal PC_OUT_NEXT: std_logic_vector(31 downto 0) := (others => '0');
+signal PC_CURRENT, PC_INCR, PC_FEEDBACK, PC_OUT_NEXT: std_logic_vector(31 downto 0) := (others => '0'); --Old PC instruction
 
 signal INSTR_ADDR: integer := 0;
-signal INSTR_BYTE: std_logic_vector(7 downto 0);
+
 signal WAIT_REQ: std_logic;
 
 signal INSTR_AUX: std_logic_vector(31 downto 0); --Current instruction
