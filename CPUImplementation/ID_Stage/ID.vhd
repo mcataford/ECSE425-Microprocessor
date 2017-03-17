9e523a87f928526d6961f4cd7941f01c9f5193ee
Simon Reynders <Simon Reynders>
2017-03-17 01:04:39 -0400

    ID modified to reflect pipeline diagram in project documentation

--- CPUImplementation/ID_Stage/ID.vhd
+++ CPUImplementation/ID_Stage/ID.vhd
@@ -10,18 +10,26 @@ entity ID is
         CLOCK : in std_logic;
         INSTRUCTION : in std_logic_vector (31 downto 0);
         PC_IN : in std_logic_vector (31 downto 0);
+        WB_REG_IN : std_logic_vector (4 downto 0);
         WB_DATA : in std_logic_vector (31 downto 0);
         WB_HILO : in std_logic_vector(63 downto 0);
 
+        --Control Signals In--
+        CONTROL_REG_WRITE_IN : out std_logic;
+
         ---Control Signals---
-        CONTROL_BRANCH : out std_logic;
-        CONTROL_MEM_TO_REG : out std_logic;
-        CONTROL_MEM_READ : out std_logic;
+        CONTROL_BRANCH_OUT,
+        CONTROL_MEM_READ_OUT,
+        CONTROL_MEM_TO_REG_OUT,
+        CONTROL_MEM_WRITE_OUT,
+        CONTROL_ALU_SRC_OUT,
+        CONTROL_REG_WRITE_OUT : out std_logic;
+
         CONTROL_ALU_OP : out std_logic_vector(3 downto 0);
-        CONTROL_MEM_WRITE : out std_logic;
-        CONTROL_ALU_SRC : out std_logic;
 
         ---Data Outputs---
+        WB_REG_OUT : out std_logic_vector(4 downto 0);
+
         PC_OUT : out std_logic_vector (31 downto 0);
         SIGN_EXTENDED_OUT : out std_logic_vector (31 downto 0);
         REG_OUT1 : out std_logic_vector (31 downto 0);
@@ -32,23 +40,7 @@ end ID;
 
 
 architecture arch of ID is
-
-    signal REG_DEST_MUX_OUT : std_logic_vector (4 downto 0);
-
-    --DATAREGISTER sigals --
-    signal rr1, rr2, wr : std_logic_vector(4 downto 0);
-    signal wl, wh, pcin : std_logic_vector(31 downto 0);
-    signal cl,crw,cgh,cgl : std_logic;
-    signal rdo1, rdo2 : std_logic_vector(31 downto 0);
-
     --CONTROL--
-    signal CTRL_BRANCH : std_logic;
-    signal CTRL_MEM_READ : std_logic;
-    signal CTRL_MEM_TO_REG : std_logic;
-    signal CTRL_ALU_OP : std_logic_vector(3 downto 0);
-    signal CTRL_MEM_WRITE : std_logic;
-    signal CTRL_ALU_SRC : std_logic;
-    signal CTRL_REG_WRITE : std_logic;
     signal CTRL_GET_HI : std_logic;
     signal CTRL_GET_LO : std_logic;
     signal CTRL_LINK : std_logic;
@@ -70,14 +62,11 @@ architecture arch of ID is
             ---Control Signals---
             CONTROL_LINK : in std_logic;
             CONTROL_REG_WRITE : in std_logic;
-            CONTROL_GET_HI : in std_logic;
-            CONTROL_GET_LO : in std_logic;
             
             ---Outputs---
-
             READ_DATA_OUT1 : out std_logic_vector (31 downto 0);
             READ_DATA_OUT2 : out std_logic_vector (31 downto 0)
-            );
+        );
     end component;
 
     component MUX_5BIT is
@@ -92,7 +81,6 @@ architecture arch of ID is
         port(
             ---Inputs---
             EXTEND_IN : in std_logic_vector (15 downto 0);
-
             ---Outputs---
             EXTEND_OUT : out std_logic_vector (31 downto 0)
         );
@@ -121,7 +109,7 @@ architecture arch of ID is
     begin
 
         REG_DEST_MUX : MUX_5BIT
-            port map(INSTRUCTION(20 downto 16), INSTRUCTION(15 downto 11), CTRL_REG_DEST, REG_DEST_MUX_OUT);
+            port map(INSTRUCTION(20 downto 16), INSTRUCTION(15 downto 11), CTRL_REG_DEST, WB_REG_OUT);
 
         EXTENDER : SIGNEXTENDER
             port map(INSTRUCTION(15 downto 0), SIGN_EXTENDED_OUT);
@@ -131,12 +119,12 @@ architecture arch of ID is
                 CLOCK,
                 INSTRUCTION(25 downto 21),
                 INSTRUCTION(20 downto 16),
-                REG_DEST_MUX_OUT,
+                WB_REG_IN,
                 WB_DATA,
                 WB_HILO,
                 PC_IN,
                 CTRL_LINK,
-                CTRL_REG_WRITE, 
+                CONTROL_REG_WRITE_IN, 
                 CTRL_GET_HI,
                 CTRL_GET_LO,
                 REG_OUT1,
@@ -148,24 +136,18 @@ architecture arch of ID is
                 INSTRUCTION(31 downto 26),
                 INSTRUCTION(5 downto 0),
                 CTRL_REG_DEST,
-                CTRL_BRANCH,
-                CTRL_MEM_READ,
-                CTRL_MEM_TO_REG,
-                CTRL_ALU_OP,
-                CTRL_MEM_WRITE,
-                CTRL_ALU_SRC,
-                CTRL_REG_WRITE,
+                CONTROL_BRANCH_OUT,
+                CONTROL_MEM_READ_OUT,
+                CONTROL_MEM_TO_REG_OUT,
+                CONTROL_ALU_OP_OUT,
+                CONTROL_MEM_WRITE_OUT,
+                CONTROL_ALU_SRC_OUT,
+                CONTROL_REG_WRITE_OUT,
                 CTRL_GET_HI,
                 CTRL_GET_LO,
                 CTRL_LINK
             );
 
-	    CONTROL_BRANCH <= CTRL_BRANCH;
-        CONTROL_MEM_TO_REG <= CTRL_MEM_TO_REG;
-        CONTROL_MEM_READ <= CTRL_MEM_READ;
-        CONTROL_ALU_OP <= CTRL_ALU_OP;
-        CONTROL_MEM_WRITE <= CTRL_MEM_WRITE;
-        CONTROL_ALU_SRC <= CTRL_ALU_SRC;
         PC_OUT <= PC_IN;
 
 end arch;
