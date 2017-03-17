9dbec505486ff7b1eca28fb50ca295b9edf1a826
mcataford <c.marcandre@gmail.com>
2017-03-17 17:47:28 -0400

    Documentation edits.

--- CPUImplementation/General_blocks/IF_ID_REGISTER.vhdl
+++ CPUImplementation/General_blocks/IF_ID_REGISTER.vhdl
@@ -5,32 +5,35 @@ use ieee.std_logic_1164.all;
 entity IF_ID_REGISTER is
     port(
         --Inputs--
-        --Change as required--
- 
+	CLOCK: in std_logic; 
         PC_IN,
-        INSTRUCTION_IN : in std_logic_vector(31 downto 0);
+        INSTR_IN : in std_logic_vector(31 downto 0);
         --Outputs--
-        --Change as required--
         PC_OUT,
-        INSTRUCTION_OUT : out std_logic_vector(31 downto 0)
+        INSTR_OUT : out std_logic_vector(31 downto 0):= (others => '0')
     );
 
 end IF_ID_REGISTER;
 
 
 architecture arch of IF_ID_REGISTER is
-    
-    signal values : std_logic_vector(63 downto 0);
+
+    signal PC_MEM, INSTR_MEM: std_logic_vector(31 downto 0) := (others => '0');
 
     begin
 
-        pipeline_buffer : process(CLOCK)
-            if(falling_edge(CLOCK)) then
-                values(31 downto 0) <= PC_IN;
-                values(63 downto 32) <= INSTRUCTION_IN;
-            end if;
+        process(CLOCK)
+	
+	begin
 
-        PC_OUT <= values(31 downto 0);
-        INSTRUCTION_OUT <= values(63 downto 32);
+            if rising_edge(CLOCK) then
+                INSTR_OUT <= INSTR_MEM;
+		PC_OUT <= PC_MEM;
+	
+		PC_MEM <= PC_IN;
+		INSTR_MEM <= INSTR_IN;
+
+            end if;
+	end process;
 
 end arch;
