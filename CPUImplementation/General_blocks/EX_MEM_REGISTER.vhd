d3e7866d64e04b3b1e272c09c02e9c57dd037623
Simon Reynders <Simon Reynders>
2017-03-17 01:14:19 -0400

    compilation errors fixed

--- CPUImplementation/General_blocks/EX_MEM_REGISTER.vhd
+++ CPUImplementation/General_blocks/EX_MEM_REGISTER.vhd
@@ -49,32 +49,34 @@ architecture arch of EX_MEM_REGISTER is
     begin
 
         pipeline_buffer : process(CLOCK)
-            if(falling_edge(CLOCK)) then
-                control(0) <= CONTROL_BRANCH_IN;
-                control(1) <= CONTROL_ZERO_IN;
-                control(2) <= CONTROL_MEM_READ_IN;
-                control(3) <= CONTROL_MEM_WRITE_IN;
-                control(4) <= CONTROL_REG_WRITE_IN;
-                control(5) <= CONTROL_MEM_TO_REG_IN;
-            
-                registers <= REG_WRITE_IN;
+	        begin
+                if(falling_edge(CLOCK)) then
+                    control(0) <= CONTROL_BRANCH_IN;
+                    control(1) <= CONTROL_ZERO_IN;
+                    control(2) <= CONTROL_MEM_READ_IN;
+                    control(3) <= CONTROL_MEM_WRITE_IN;
+                    control(4) <= CONTROL_REG_WRITE_IN;
+                    control(5) <= CONTROL_MEM_TO_REG_IN;
                 
-                values(31 downto 0) <= PC_IN;
-                values(63 downto 32) <= ALU_RESULT_IN;
-                values(95 downto 63) <= WRITE_DATA_IN;
-            end if;
+                    registers <= REG_WRITE_IN;
+                    
+                    values(31 downto 0) <= PC_IN;
+                    values(63 downto 32) <= ALU_RESULT_IN;
+                    values(95 downto 63) <= WRITE_DATA_IN;
+                end if;
+            end process pipeline_buffer;
 
-        CONTROL_BRANCH_OUT <= control(0);
-        CONTROL_ZERO_OUT <= control(1);
-        CONTROL_MEM_READ_OUT <= control(2);
-        CONTROL_MEM_WRITE_OUT <= control(3);
-        CONTROL_REG_WRITE_OUT <= control(4);
-        CONTROL_MEM_TO_READ_OUT <= control(5);
+            CONTROL_BRANCH_OUT <= control(0);
+            CONTROL_ZERO_OUT <= control(1);
+            CONTROL_MEM_READ_OUT <= control(2);
+            CONTROL_MEM_WRITE_OUT <= control(3);
+            CONTROL_REG_WRITE_OUT <= control(4);
+            CONTROL_MEM_TO_REG_OUT <= control(5);
 
-        REG_WRITE_OUT <= registers;
+            REG_WRITE_OUT <= registers;
 
-        PC_OUT <= values(31 downto 0);
-        ALU_RESULT_OUT <= values(63 downto 32);
-        WRITE_DATA_IN <= values(95 downto 63);
+            PC_OUT <= values(31 downto 0);
+            ALU_RESULT_OUT <= values(63 downto 32);
+            WRITE_DATA_OUT <= values(95 downto 64);
 
 end arch;
